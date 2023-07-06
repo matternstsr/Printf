c2383f0d9744241612a3e4332ec3319c9469bb42
matternstsr <matternstsr@gmail.com>
2023-07-06 14:08:38 -0700

    fixed main.h

--- main.h
+++ main.h
@@ -7,8 +7,9 @@ void print_percent(va_list args __attribute__((unused)));
 void print_int(va_list args);
 int _putchar(char c);
 void _puts(char *str);
-void print_number(int n);
 int _printf(const char *format, ...);
+void _recursion_integer(int a);
+int _print_a_integer(va_list args);
 
 /**
  * struct format_type - Struct op
