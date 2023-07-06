213e2e7e41361e73e0019f8bb033ed3924248593
matternstsr <matternstsr@gmail.com>
2023-07-06 13:20:02 -0700

    prodtotypes were missing the ;

--- main.h
+++ main.h
@@ -1,14 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_char(va_list args)
-void print_string(va_list args)
-void print_percent(va_list args __attribute__((unused)))
-void print_int(va_list args)
-int _putchar(char c)
-void _puts(char *str)
-void print_number(int n)
-int _printf(const char *format, ...)
+void print_char(va_list args);
+void print_string(va_list args);
+void print_percent(va_list args __attribute__((unused)));
+void print_int(va_list args);
+int _putchar(char c);
+void _puts(char *str);
+void print_number(int n);
+int _printf(const char *format, ...);
 
 /**
  * struct format_type - Struct op
