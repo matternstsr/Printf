f45f2211ce8bba41504079769c2b50809112a26e
connortrue <connor.true.94@gmail.com>
2023-07-08 16:28:05 -0700

    commented the hotfix in

--- functions.c
+++ functions.c
@@ -88,6 +88,7 @@ int _print_integer(va_list args)
 		_write('-');
 		/* Converts the integer to positive. */
 		mkunsnd = mkunsnd * -1;
+		/* Converts data type to unsigned */
 		check = mkunsnd;
 		/* Updates the counter. */
 		ct++;
