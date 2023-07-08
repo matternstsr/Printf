f7f8d1d0517ea97e6faa003e73d3a8e2d120dfbc
connortrue <connor.true.94@gmail.com>
2023-07-08 15:55:49 -0700

    expanded on comments

--- functions.c
+++ functions.c
@@ -6,11 +6,19 @@
   * _print_char - Prints a character
   * @args: A list of variadic arguments
   *
+  * This function prints a character.
+  *
   * Return: The length of the character after processing
   */
 int _print_char(va_list args)
 {
-	_write(va_arg(args, int));
+	/* Gets the character from the va_list. */
+	int c = va_arg(args, int);
+
+	/* Writes the character to stdout. */
+	_write(c);
+
+	/* Returns the length of the character. */
 	return (1);
 }
 
@@ -18,36 +26,49 @@ int _print_char(va_list args)
   * _print_string - Prints a string
   * @args: A list of variadic arguments
   *
+  * This function prints a string.
+  *
   * Return: The length of the string after processing
   */
 int _print_string(va_list args)
 {
+	/* Gets the string from the va_list. */
 	char *arg = va_arg(args, char *);
 	int i = 0;
 
+	/* Checks if the string is NULL. */
 	if (arg != NULL)
 	{
+		/* Iterates over the string, writing each character to stdout. */
 		while (arg[i])
 		{
+			/* Writes the character to stdout. */
 			_write(arg[i]);
+			/* Increments the counter. */
 			i++;
 		}
 
+		/* Returns the length of the string. */
 		return (i);
 	}
 
+	/* If the string is NULL, prints "(null)" to stdout. */
 	_write('(');
 	_write('n');
 	_write('u');
 	_write('l');
 	_write('l');
 	_write(')');
+
+	/* Returns the length of the string, which is 6. */
 	return (6);
 }
 
 /**
   * _print_integer - Prints a integer
   * @args: A list of variadic arguments
+  *
+  * This function prints an integer.
   *
   * Return: The length of the string after processing
   */
@@ -56,28 +77,42 @@ int _print_integer(va_list args)
 	int ct = 1, mkunsnd = 0;
 	unsigned int check = 0;
 
+	/* Gets the integer from the va_list. */
 	check = va_arg(args, int);
 	mkunsnd = check;
+
+	/* Checks if the integer is negative. */
 	if (mkunsnd < 0)
 	{
+		/* Prints a '-' character to stdout. */
 		_write('-');
+		/* Converts the integer to positive. */
 		mkunsnd = mkunsnd * -1;
-		check = mkunsnd;
-		ct += 1;
-	}
-	while (check > 9)
-	{
-		check = check / 10;
+		/* Updates the counter. */
 		ct++;
 	}
 
+	/* Iterates while the integer is greater than 9. */
+	while (check > 9)
+	{
+		/* Divides the integer by 10. */
+		check = check / 10;
+		/* Increments the counter. */
+		ct++;
+	}
+
+	/* Recursively calls the function to print the integer. */
 	_integer_printer(mkunsnd);
+
+	/* Returns the length of the integer. */
 	return (ct);
 }
 
 /**
-  * _integer_printer - Prints an integer
+  * _integer_printer - Prints an integer recursively
   * @a: int brought in for print
+  *
+  * This function prints an integer recursively.
   *
   * Return: Nothing
   */
@@ -85,8 +120,16 @@ void _integer_printer(int a)
 {
 	unsigned int intprnt;
 
+	/* Gets the integer. */
 	intprnt = a;
+
+	/* Checks if the integer is greater than 9. */
 	if (intprnt / 10)
+	{
+		/* Recursively calls the function to print the integer. */
 		_integer_printer(intprnt / 10);
+	}
+
+	/* Prints the last digit of the integer. */
 	_write(intprnt % 10 + '0');
 }
--- our_formats.c
+++ our_formats.c
@@ -5,42 +5,51 @@
  * _our_formats - format selection
  * @format: The formats that are chosen for printing
  * @args: A list of variadic arguments
+ *
+ * This function iterates over the format string, looking for '%' characters.
+ * If a '%' character is found, the function checks to see if the next char
+ * is a valid format specifier. If it is, the function calls the corresponding
+ * function to print the specifier.
+ *
  * Return: The length of the format
  */
 int _our_formats(const char *format, va_list args)
 {
-	int ct = 0;
-	int i = 0;
+	int ct = 0; /* The length of the format */
+	int i = 0; /* The current index in the format string */
 
+	/* Iterate over the format string */
 	while (format && format[i])
 	{
+		/* If the current character is '%' */
 		if (format[i] == '%')
 		{
+			/* Return -1 if null terminator */
 			if (format[i + 1] == '\0')
 				return (-1);
-
 			i++;
 
+			/* Skip any whitespace */
 			while (format[i] == ' ')
 				i++;
 
+			/* Print a % and move on */
 			if (format[i] == '%')
 				ct += _write(format[i]);
 
+			/* If the next character is not a valid format specifier, print '%' */
 			if (_charchecker(format[i]) == 0)
 			{
 				ct += _write('%');
 				ct += _write(format[i]);
 			}
+			/* Otherwise, call the corresponding function to print the specifier */
 			else
-			{
 				ct += _printspec(format[i], args);
-			}
 		}
+		/* Otherwise, just print the current character */
 		else
-		{
 			ct += _write(format[i]);
-		}
 
 		i++;
 	}
@@ -52,11 +61,16 @@ int _our_formats(const char *format, va_list args)
  * _printspec - Prints a valid specifier
  * @format: The specifier to prints
  * @args: A list of variadic arguments
+ *
+ * This function takes a format specifier and a list of variadic arguments,
+ * and prints the specifier using the corresponding function.
+ *
  * Return: The length of the specifier
  */
 int _printspec(char format, va_list args)
 {
-	int i = 0, length = 0;
+	int i = 0; /* The current index in the specifier array */
+	int length = 0; /* The length of the specifier */
 	spc_dt _types[] = {
 		{"c", _print_char},
 		{"s", _print_string},
@@ -65,9 +79,12 @@ int _printspec(char format, va_list args)
 		{NULL, NULL}
 	};
 
+	/* Iterate over the specifier array */
 	while (_types[i].specifier)
 	{
+		/* If the current specifier matches the format specifier */
 		if (*_types[i].specifier == format)
+			/* Print the specifier using the corresponding function */
 			length = _types[i].f(args);
 
 		i++;
@@ -79,6 +96,9 @@ int _printspec(char format, va_list args)
 /**
  * _charchecker - validate the type
  * @_type: character to be comparate
+ *
+ * This function takes a character and checks if it is a valid format spec.
+ *
  * Return: 1 if the character is equal to a type
  */
 int _charchecker(char _type)
@@ -86,11 +106,16 @@ int _charchecker(char _type)
 	char _types[] = {'c', 's', 'd', 'i', '%'};
 	int i = 0;
 
+	/* Iterate over the specifier array */
 	while (_types[i])
 	{
+		/* If the current character matches a specifier */
 		if (_types[i] == _type)
 			return (1);
+
 		i++;
 	}
+
+	/* Return 0 if the character is not a specifier */
 	return (0);
 }
